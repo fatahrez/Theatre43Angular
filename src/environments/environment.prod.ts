cb4943289a90f2ce51518c58fed9981931cbf616
fatahrez <fertahrez@gmail.com>
2021-07-21 16:32:32 +0300

    remove dist from gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'http://quotes.stormconsultancy.co.uk/random.json',
+  moviedb_api_url: 'https://api.themoviedb.org/4/',
+  api_key: '1691caa621ca96a64ea58ab55fffa132'
 };
