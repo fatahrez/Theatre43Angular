1968a4701fbc11523ca5a0e3ec58614ebda855b4
fatahrez <fertahrez@gmail.com>
2021-07-25 01:04:14 +0300

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,7 @@ export const environment = {
   production: false,
   api_url: 'http://quotes.stormconsultancy.co.uk/random.json',
   moviedb_api_url: 'https://api.themoviedb.org/',
-  api_key: '1691caa621ca96a64ea58ab55fffa132',
-  youtube_key: 'AIzaSyDOsqqaas7MfrTGNFUQmaTYtQtbZU0Lwq8',
-  youtube_base_url: 'https://www.googleapis.com/youtube/v3/'
+  api_key: '1691caa621ca96a64ea58ab55fffa132'
 };
 
 /*
